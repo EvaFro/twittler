3407aa3940df091e6693f7d1aaa8a5d3b06b8668
EvaFro <frolli.studios@gmail.com>
2017-12-26 19:11:06 -0700

    Added time code to loop of bodyCode.js

--- bodyCode.js
+++ bodyCode.js
@@ -1,14 +1,28 @@
 $(document).ready(function(){
-	var $body = $('body');
-	$body.html('');
+var $main = $('.main')
 
-	var index = streams.home.length - 1;
-	while(index >= 0){
-		var tweet = streams.home[index];
-		var $tweet = $('<div></div>');
-		$tweet.text('@' + tweet.user + ': ' + tweet.message);
-		$tweet.appendTo($body);
-		index -= 1;
-	}
+var $tweetFeed = $('<div></div>');
+$tweetFeed.appendTo($main);
+$tweetFeed.html('');
 
+var index = streams.home.length - 1;
+while(index >= 0){
+  var tweet = streams.home[index];
+  var $tweet = $('<div></div>');
+  $tweet.text('@' + tweet.user + ': ' + tweet.message);
+  $tweet.appendTo($tweetFeed);
+  
+  //Add time stamp to our tweets
+  $tweetTime = $('<span></span>');
+  $tweetTime.addClass('timestamp');
+  //make easy to read
+  readableTime = moment(tweet.created_at).fromNow();
+  $tweetTime.text(readableTime);
+  $tweetTime.appendTo($tweet);
+	
+  //add tweet to our tweetFeed
+  $tweet.appendTo($tweetFeed);
+  
+  index -= 1;
+}
 });
